674fc348162047154d34ccb2c4650f8e5b183ad0
l-palermo <l-palermo@hotmail.it>
2020-06-18 16:31:18 +0100

    add babel react config

---
+++ src/components/tool-logos/tool-logos.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+import logo from "../../assets/pnpm.png";
+import "./tool-logos.css";
+
+const ToolLogos = () => {
+  return <img className="logo" src={logo} alt="image" />;
+};
+
+export default ToolLogos;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,5 @@
-import h3 from "./app";
-import pnpm from "./assets/pnpm.png";
-import "./app.css";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './app';
 
-const h1 = document.createElement("h1");
-h1.innerHTML = "Hello world";
-const root = document.getElementById("root");
-root.appendChild(h1);
-root.appendChild(h3());
-
-const img = document.createElement("img");
-img.setAttribute("class", "svg");
-img.setAttribute("src", pnpm);
-img.setAttribute("alt", "pnpm logo");
-
-root.appendChild(img);
+ReactDOM.render(<App />, document.getElementById('root'));
