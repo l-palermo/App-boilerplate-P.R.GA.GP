e8f4dd69fb9c23ffc0a12d5dda4facb8dad61b7e
l-palermo <l-palermo@hotmail.it>
2020-06-30 13:38:57 +0100

    add git actions and sort rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -10,49 +10,60 @@ import babel from "@rollup/plugin-babel";
 import resolve from "@rollup/plugin-node-resolve";
 import commonJS from "@rollup/plugin-commonjs";
 
-export default {
-  input: "src/index.js",
-  output: {
-    file: "dist/bundle.js",
-  },
-  // external: ["react", "react-dom"],
-  plugins: [
-    // Locate and bundle third-party dependencies in node_modules
-    resolve(),
-    babel({
-      exclude: "node_modules/**",
-      presets: ["@babel/env", "@babel/preset-react"],
-    }),
-    // convert CommonJS modules to ES6
-    commonJS({
-      include: "node_modules/**",
-    }),
-    // takes care of node global variables
-    globals(),
-    // dev-server
-    serve({
-      open: true,
-      openPage: "/",
-      contentBase: "dist",
-      port: 4000,
-    }),
-    // styles(),
-    css(),
-    // custom html template
-    htmlTemplate({
-      template: "public/index.html",
-      target: "dist/index.html",
-    }),
-    // clear the dist folder after each build
-    clear({
-      targets: ["dist"],
-      watch: true,
-    }),
-    // livereload after changes, very handy, no page refresh for css
-    livereload(),
-    // allow to import image type of assets
-    image(),
-    // allow to import assets as data:urls
-    url(),
-  ],
+export default (settings) => {
+  console.log(settings.environment);
+  const env = settings.environment;
+
+  const servePlug = !env
+    ? serve({
+        open: true,
+        openPage: "/",
+        contentBase: "dist",
+        port: 4000,
+      })
+    : null;
+
+  const liveReload = !env ? livereload() : null;
+
+  return {
+    input: "src/index.js",
+    output: {
+      file: "dist/bundle.js",
+    },
+    // external: ["react", "react-dom"],
+    plugins: [
+      // Locate and bundle third-party dependencies in node_modules
+      resolve(),
+      babel({
+        exclude: "node_modules/**",
+        presets: ["@babel/env", "@babel/preset-react"],
+      }),
+      // convert CommonJS modules to ES6
+      commonJS({
+        include: "node_modules/**",
+      }),
+      // takes care of node global variables
+      globals(),
+      // dev-server
+      servePlug,
+      // styles(),
+      css(),
+      // custom html template
+      htmlTemplate({
+        template: "public/index.html",
+        target: "dist/index.html",
+      }),
+      // clear the dist folder after each build
+      clear({
+        targets: ["dist"],
+        watch: true,
+      }),
+      // livereload after changes, very handy, no page refresh for css
+      liveReload,
+      // allow to import image type of assets
+      image(),
+      // allow to import assets as data:urls
+      url(),
+    ],
+  };
 };
